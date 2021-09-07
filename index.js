77254b96e064b763eb717ca9b1d63a0a61389157
George Gosden <gosdeng@gmail.com>
2021-09-07 12:33:56 +0100

    added socket.io

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
+//socketIO stuff
+const {Server} = require("socket.io");
+const io = new Server(server);
+
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + '/frontend/index.html');
 });
 
+//socket-watch
+io.on("connection", (socket) =>{
+    console.log("a user connected");
+    socket.on("disconnect", () =>{
+        console.log("user disconnected");
+    });
+});
+
 server.listen(3000, () =>{
     console.log("listening on *:3000")
-})
+});
